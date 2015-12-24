30ae70608d7df744eea2faaa7918a04a9383225a
lichunqiang <light-li@hotmail.com>
2015-12-24 13:24:09 +0800

    nodejs

---
+++ node/nodejs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+pkg=node-v5.3.0-linux-x64
+
+cd /usr/local
+
+wget https://nodejs.org/dist/v5.3.0/$pkg.tar.gz
+
+tar -zxvf $pkg.tar.gz
+
+rm -f $pkg.tar.gz
+
+mv $pkg nodejs
+
+
+ln -s /usr/local/nodejs/bin/node /usr/local/bin/node
+ln -s /usr/local/nodejs/bin/npm /usr/local/bin/npm
