4be228d71a603ac70f7fe9ff0b92ec135f939b9d
__FresHmaN <light-li@hotmail.com>
2017-08-14 00:41:22 +0800

    Update 3-2-intl.sh

--- nginx-mysql/3-2-intl.sh
+++ nginx-mysql/3-2-intl.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 dest=/usr/local/src
-icu=icu4c-56_1-src.tgz
+icu=icu4c-59_1-src.tgz
 intlVer=intl-3.0.0
 intl=$intlVer.tgz
 
 # install
 cd $dest
 
-wget https://raw.githubusercontent.com/mythteam/shell/master/pkgs/$icu
-wget https://raw.githubusercontent.com/mythteam/shell/master/pkgs/$intl
+wget http://download.icu-project.org/files/icu4c/59.1/$icu
+#wget https://raw.githubusercontent.com/mythteam/shell/master/pkgs/$icu
+#wget https://raw.githubusercontent.com/mythteam/shell/master/pkgs/$intl
 
 # install icu
 tar zxvf $icu
@@ -20,15 +21,15 @@ cd icu/source
 make
 make install
 
-cd $dest
-tar zxvf $intl
-cd $intlVer
-/usr/local/php/bin/phpize
+#cd $dest
+#tar zxvf $intl
+#cd $intlVer
+#/usr/local/php/bin/phpize
 
-./configure --with-php-config=/usr/local/php/bin/php-config
+#./configure --with-php-config=/usr/local/php/bin/php-config
 
-make
-make install
+#make
+#make install
 
 
 ######################
