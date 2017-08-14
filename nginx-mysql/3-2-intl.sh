366888bbe989ef68b4e7185d420ae46e3aed3d63
__FresHmaN <light-li@hotmail.com>
2017-08-14 19:37:33 +0800

    Update 3-2-intl.sh

--- nginx-mysql/3-2-intl.sh
+++ nginx-mysql/3-2-intl.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 dest=/usr/local/src
-icu=icu4c-59_1-src.tgz
+icu=icu4c-56_1-src.tgz
 intlVer=intl-3.0.0
 intl=$intlVer.tgz
 
 # install
 cd $dest
 
-wget http://download.icu-project.org/files/icu4c/59.1/$icu
+wget http://download.icu-project.org/files/icu4c/56.1/$icu
 #wget https://raw.githubusercontent.com/mythteam/shell/master/pkgs/$icu
 #wget https://raw.githubusercontent.com/mythteam/shell/master/pkgs/$intl
 
